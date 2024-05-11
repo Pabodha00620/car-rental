c29892a68c14038cd8cb6f23eeb6129fa2e2c6de
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-05-11 19:20:47 +0530

    2nd commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -63,14 +63,80 @@ const Header =() =>{
             <div className='header_middle'>
                 <Container>
                     <Row>
-                        <Col lg="4" md="3" sm="4">
+                    <Col lg="4" md="3" sm="4">
+                        <div className='logo'>
+                            <h1>
+                                <Link to="/home" className='d-flex align-items-center gap-2'>
+                                    <i class="ri-car-line">
+                                        <span>
+                                            Rent Car <br/> Service
+                                        </span>
+                                    </i>
+                                </Link>
+                            </h1>
+                        </div>
+                    </Col>
+
+
+                    <Col lg="4" md="3" sm="4">
                             <div className='header_location d-flex align-items-center gap-2'>
                                 <span>
                                     <i class="ri-earth-line"></i>
                                 </span>
+                                <div className='header_location-content'>
+                                    <h4>Bangladesh</h4>
+                                    <h6>Sylhet City, Bangladesh</h6>
+                                </div>
                             </div>
                         </Col>
+
+                        <Col
+                         lg="2"
+                         md="3"
+                         sm="0"
+                         className='d-flex align-items-center justify-content-end'>
+                            <button className='header_btn btn'>
+                                <Link to="/contact">
+                                    <i class="ri-phone-line"></i> Request a call
+                                </Link>
+                            </button>
+                         </Col>
                     </Row>
+                </Container>
+            </div>
+
+
+            <div className='main_navbar'>
+                <Container>
+                    <div className='navigation_wrapper d-flex align-items-center justify-content-between'>
+                        <span className='mobile_menu'>
+                            <i class="ri-menu-line" onClick={toggleMenu}></i>
+                        </span>
+
+                        <div className='navigation' ref={menuRRef} onClick={toggleMenu}>
+                            <di className="menu">
+                                {navLinks.map((item, index)=>(
+                                    <NavLink
+                                    to={item.path}
+                                    className={(navClass) =>
+                                        navClass.isActive ? "nav_active nav_item" : "nav_item"
+                                    }
+                                    key={index}>
+                                        {item.display}
+                                    </NavLink>
+                                ))}
+                            </di>
+                        </div>
+
+                        <div className='nav_right'>
+                            <div className='search_box'>
+                                <input type='text' placeholder='Search'/>
+                                <span>
+                                    <i class ="ri-search-line"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </Container>
             </div>
         </header>
