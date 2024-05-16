bade98f00edbfb8997542f7be07c743948978745
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-05-16 10:23:34 +0530

    11 Commit

--- src/Components/UI/AboutSection.jsx
+++ src/Components/UI/AboutSection.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from 'react';
 
-export default function AboutSection() {
+import { Container, Row, Col } from 'reactstrap';
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({aboutClass}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className='about_section'
+    style={
+      aboutClass === "aboutPage"
+      ? {marginTop: "0px"}
+      : {marginTop: "280px"}
+    }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className='about_section-content'>
+              <h4 className='section_subtitle'>About Us</h4>
+              <h4 className='section_title'>Welcome to car rent service</h4>
+              <p className='section_description'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatum blanditiis esse accusantium dignissimos labore
+                laborum. Veniam, corporis mollitia temporibus, in quaerat vero
+                deleniti amet dolorem repudiandae, pariatur nam dolore! Impedit
+                neque sit ad temporibus quam similique dolor ipsam praesentium
+                sunt.
+
+              </p>
+
+              <div className='about_section-item d-flex align-items-center'>
+                <p className='section_description d-flex align-items-center gap-2'>
+                  <i class="ri-checkbox-circle-line"></i>Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className='section_description d-flex align-items-center gap-2'>
+                  <i class="ri-checkbox-cricle-line"></i>Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+
+              <div className='about_section-item d-flex align-items-center'>
+                <p className='section_description d-flex align-items-center gap-2'>
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className='section_description d-flex align-items-center gap-2'>
+                  <i class="ri-checkbox-circle-line"></i>Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className='about_img'>
+              <img src={aboutImg} alt="" className='w-100' />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
--- src/Components/UI/BecomeDriverSection.jsx
+++ src/Components/UI/BecomeDriverSection.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-export default function BecomeDriverSection() {
+import "../../styles/become-driver.css";
+import { Container, Row, Col } from 'reactstrap';
+
+import driverImg from "../../assets/all-images/toyota-offer-2.png";
+
+const BecomeDriverSection = () =>{
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className='become_driver'>
+      <Container>
+        <Row>
+          <Col lg="6" md="6" sm="12" className='become_driver-img'>
+            <img src={driverImg} alt="" className='w-100' />
+          </Col>
+
+          <Col lg="6" md="6" sm="12">
+            <h2 className='section_title become_driver-title'>
+              Do You Want to Earn with Us? So Don't be Late
+            </h2>
+
+            <button className='btn become_driver-btn mt-4'>
+              Become a Driver
+            </button>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default BecomeDriverSection;
