0c9872519bb6dc9e00a69b12b0c466820660f7bc
Pabodha Gamage <pabodhadivyanjali@gmail.com>
2024-05-17 11:48:56 +0530

    13 Commit

--- src/Components/UI/BookingForm.jsx
+++ src/Components/UI/BookingForm.jsx
@@ -7,5 +7,71 @@ const BookingForm = () =>{
   const submitHandler = (event) =>{
     event.preventDefault();
   };
-}
+
+  return (
+    <From onSubmit={submitHandler}>
+      <FormGroup className='booking_form d-inline-block me-4 mb-4'>
+        <input type='text' placeholder='First Name' />
+      </FormGroup>
+
+      <FormGroup className='booking_form d-inline-block ms-1 mb-4'>
+        <input type='email' placeholder='Email' />
+      </FormGroup>
+      <FormGroup className='booking_form d-inline-block me-4 mb-4'>
+        <input type="number" placeholder="Phone Number" />
+      </FormGroup>
+
+      <FormGroup className='booking_form d-inline-block ms-1 mb-4'>
+        <input type="text" placeholder='From Address' />
+      </FormGroup>
+      <FormGroup className='booking_form d-inline-block ms-1 mb-4'>
+      <input type="text" placeholder='To Address' />
+      </FormGroup>
+
+      <FormGroup className='booking_form d-inline-block me-4 mb-4'>
+        <select name="" id="">
+          <option value="1 person">1 Person</option>
+          <option value="2 person">2 Person</option>
+          <option value="3 person">3 Person</option>
+          <option value="4 person">4 Person</option>
+          <option value="5+ person">5+ Person</option>
+        </select>
+      </FormGroup>
+
+      <FormGroup className='booking_form d-inline-block ms-1 mb-4'>
+        <select name="" id="">
+        <option value="1 person">1 Person</option>
+          <option value="2 person">2 Person</option>
+          <option value="3 person">3 Person</option>
+          <option value="4 person">4 Person</option>
+          <option value="5+ person">5+ Person</option>
+        </select>
+      </FormGroup>
+
+      <FormGroup className='booking_form d-inline-block me-4 mb-4'>
+        <input type='date' placeholder='Journey Date' />
+      </FormGroup>
+      <FormGroup className='booking_form d-inline-block ms-1 mb-4'>
+        <input 
+        type='time'
+        placeholder='Journey Time'
+        className='time_picker'
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <textarea
+        rows={5}
+        className='textarea'
+        placeholder='Write'>
+        </textarea>
+      </FormGroup>
+
+    </From>
+      
+  );
+};
+
+export default BookingForm;
+
 
--- src/Components/UI/CarItem.jsx
+++ src/Components/UI/CarItem.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {Col} from "reactstrap";
+import {Link} from "react-router-dom";
+import "../../styles/car-item.css";
 
-export default function CarItem() {
-  return (
-    <div>
-      
-    </div>
+const CarItem = (props) => {
+  const {imgUrl, model, carName, automatic, speed, price} = props.item;
+
+  return(
+    <Col lg="4" md="4" sm="6" className='md-5'>
+      <div className='car_item'>
+        <div className='car_img'>
+          <img src={imgUrl} alt="" className='w-100' />
+        </div>
+
+        <div className='car_item-content mt-4'>
+          <h4 className='section_title text-center'>{carName}</h4>
+          <h6 className='rent_price text-center mt-'>
+            ${price}.00 <span>/ Day</span>
+          </h6>
+
+          <div className='car_item-info d-flex align-items-center justify-content-between mt-3 mb-4'>
+            <span className='d-flex align-items-center gap-1'>
+              <i class="ri-car-line"></i> {model}
+            </span>
+          </div>
+        </div>
+      </div>
+    </Col>
   )
 }
